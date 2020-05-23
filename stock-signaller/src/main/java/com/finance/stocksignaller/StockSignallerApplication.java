91428424f784bd5e89cab65da756429699c133be
Alvaro Velasco <avf@exerp.com>
2020-05-23 19:08:09 +0200

    First commit to add the projects to Github

---
+++ stock-signaller/src/main/java/com/finance/stocksignaller/StockSignallerApplication.java
@@ -0,0 +1,13 @@
+package com.finance.stocksignaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSignallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSignallerApplication.class, args);
+	}
+
+}
---
+++ stock-signaller/src/test/java/com/finance/stocksignaller/StockSignallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finance.stocksignaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSignallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
